0d0ca25504ab0b8fe107791cec72fd71ff4e32a9
XYZ <240981737@qq.com>
2019-07-14 19:44:37 +0800

    test

--- src/helloworld.java
+++ src/helloworld.java
@@ -3,7 +3,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD Ëæ±ã¸ÄÒ»¸Ä");
 	}
 
 }
